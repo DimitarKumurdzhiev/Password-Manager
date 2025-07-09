c25e25cafb09a26f9b32ef576734ed1f1f7c8cf8
DimitarKumurdzhiev <dimitarminkov321@gmail.com>
2025-07-09 10:34:55 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Master_passwords
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox txtWebsite;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListView listView1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtWebsite = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+
+            this.SuspendLayout();
+
+            // txtWebsite
+            this.txtWebsite.Location = new System.Drawing.Point(12, 12);
+            this.txtWebsite.Name = "txtWebsite";
+            this.txtWebsite.Size = new System.Drawing.Size(200, 22);
+
+            // txtUsername
+            this.txtUsername.Location = new System.Drawing.Point(12, 40);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 22);
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(12, 68);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 22);
+            this.txtPassword.UseSystemPasswordChar = true;
+
+            // btnSave
+            this.btnSave.Location = new System.Drawing.Point(12, 96);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(200, 30);
+            this.btnSave.Text = "Add Password";
+            this.btnSave.UseVisualStyleBackColor = true;
+
+            // listView1
+            this.listView1.Location = new System.Drawing.Point(230, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(550, 300);
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Columns.Add("Website", 150);
+            this.listView1.Columns.Add("Username", 150);
+            this.listView1.Columns.Add("Password", 200);
+
+            // MainForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtWebsite);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.listView1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Master_passwords
+{
+    public partial class MainForm : Form
+    {
+        private List<PasswordEntry> entries = new List<PasswordEntry>();
+
+        // Просто за пример, фиксиран ключ и IV (16 байта)
+        private readonly byte[] key = Encoding.UTF8.GetBytes("1234567890123456");
+        private readonly byte[] iv = Encoding.UTF8.GetBytes("6543210987654321");
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            btnSave.Click += BtnSave_Click;
+
+            SetupListView();
+        }
+
+        private void SetupListView()
+        {
+            // Ако не си го добавил в Designer-а, направи го тук
+            listView1.Columns.Clear();
+            listView1.Columns.Add("Website", 150);
+            listView1.Columns.Add("Username", 150);
+            listView1.Columns.Add("Password", 150);
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            string website = txtWebsite.Text.Trim();
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(website) || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Моля, попълнете всички полета.");
+                return;
+            }
+
+            string encryptedPassword = EncryptString(password);
+
+            entries.Add(new PasswordEntry()
+            {
+                Website = website,
+                Username = username,
+                EncryptedPassword = encryptedPassword
+            });
+
+            RefreshListView();
+
+            // Изчистваме полетата за нов запис
+            txtWebsite.Text = "";
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+
+        private void RefreshListView()
+        {
+            listView1.Items.Clear();
+
+            foreach (var entry in entries)
+            {
+                string decryptedPassword = DecryptString(entry.EncryptedPassword);
+
+                ListViewItem item = new ListViewItem(entry.Website);
+                item.SubItems.Add(entry.Username);
+                item.SubItems.Add(decryptedPassword);
+
+                listView1.Items.Add(item);
+            }
+        }
+
+        private string EncryptString(string plainText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+
+                byte[] encryptedBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+
+        private string DecryptString(string cipherText)
+        {
+            try
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.IV = iv;
+
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+                    byte[] decryptedBytes = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+
+                    return Encoding.UTF8.GetString(decryptedBytes);
+                }
+            }
+            catch
+            {
+                return "[Грешка при декриптиране]";
+            }
+        }
+    }
+
+    public class PasswordEntry
+    {
+        public string Website { get; set; }
+        public string Username { get; set; }
+        public string EncryptedPassword { get; set; }
+    }
+}
+
